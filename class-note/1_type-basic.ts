2184ac1c64bdf74db778eabc735a2ee29631461b
duck <godqhr2256@gmail.com>
2022-08-14 21:35:08 +0900

    typescript basic type

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,25 @@
+var str: string = "hello";
+
+let num: number = 10;
+
+let arr: Array<number> = [1, 2, 3];
+let arr2: Array<string> = ["capt", "thor", "hulk"];
+
+let items: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number, string] = ["gangnam", 10, "gwangyo"];
+
+// TS 객체
+let obj: object = {};
+let person: object = {
+  name: "capt",
+  age: 100,
+};
+let man: { name: string; age: number } = {
+  name: "capt",
+  age: 20,
+};
+
+//TS 진위값
+let show: boolean = true;
